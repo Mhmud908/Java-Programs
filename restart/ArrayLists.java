151e8fb83fcf96ae2238572c845b3fe1f01d6143
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-02-18 02:36:06 +0500

    Arralist added

---
+++ restart/ArrayLists.java
@@ -0,0 +1,33 @@
+package restart;
+
+import java.util.ArrayList;
+
+class ArrayLists {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+            list.add("Mehmood");
+            list.add("Hassan");
+        System.out.println(list);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+            list2.add(1);
+            list2.add(2);
+            list2.add(5);
+            list2.add(9);
+        System.out.println(list2);
+
+        int value = list2.get(3);
+        System.out.println(value);
+
+        list2.remove(2);
+        System.out.println(list2);
+
+        list2.set(2, 7);
+        System.out.println(list2);
+
+        list2.add(2, 8);
+        System.out.println(list2);
+
+
+    }
+}
