827ff551829b417de5b1d74809f35db731cb259a
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-02-21 05:54:34 +0500

    Created BankService class, and added account creation method

--- BankingSystem/src/service/BankService.java
+++ BankingSystem/src/service/BankService.java
@@ -1,5 +1,24 @@
 package BankingSystem.src.service;
 
+import java.util.ArrayList;
+
+import BankingSystem.src.model.BankAccount;
+
 public class BankService {
+
+    private ArrayList<BankAccount> accounts;
+
+    int totalTransactions;
+
+    public BankService() {
+        accounts = new ArrayList<>();
+        totalTransactions = 0;
+
+    }
+
+    public void addAccount(BankAccount account) {
+        accounts.add(account);
+    }
+
     
 }
