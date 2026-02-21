aa64bded88126579b5c2a91ebfbc8c982391a272
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-02-22 01:07:55 +0500

    Interest calculator added

--- BankingSystem/src/util/InterestCalculator.java
+++ BankingSystem/src/util/InterestCalculator.java
@@ -1,5 +1,13 @@
 package BankingSystem.src.util;
 
 public class InterestCalculator {
+
+    public static double calculateCoumpoundInterest( double principal, double rate, int years){
+        if(years == 0){
+            return principal;
+        }
+
+        return calculateCoumpoundInterest(principal, rate, years - 1) * (1 + rate / 100);
+    }
     
 }
