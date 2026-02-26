73f1b7ddcf0b225481c9a19c16328c07db5d9678
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-02-26 19:24:34 +0500

    NumberCheck addec

---
+++ NumberCheck.java
@@ -0,0 +1,17 @@
+public class NumberCheck {
+
+    public static void main(String[] args) {
+        
+        int num = 12345;
+        int sum = 0;
+        
+        while(num > 0) {
+            int digit = num % 10; // Get the last digit
+            sum += digit; // Add the digit to the sum
+            num /= 10; // Remove the last digit
+        }
+        
+        System.out.println("The sum of the digits is: " + sum);
+    }
+    
+}
