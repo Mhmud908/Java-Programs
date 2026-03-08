58e230e38b6f3fa06a48e9f39d57039bfa362ae6
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-03-08 21:31:53 +0500

    Strings folder added

---
+++ strings/PalindromeStrings.java
@@ -0,0 +1,29 @@
+package strings;
+
+public class PalindromeStrings {
+
+    public static boolean IsPalindrome(String str){
+
+        int end = str.length()-1;
+
+        for(int i = 0; i < end/2; i++){
+
+            if(str.charAt(i) != str.charAt(end-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+
+        String str = "abcdlcba";
+
+        if(IsPalindrome(str))
+            System.out.println("The string is Palindrome");
+        else
+            System.out.println("The string is Not Palindrome");
+
+    }
+    
+}
