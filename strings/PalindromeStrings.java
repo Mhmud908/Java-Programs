51f66525519c24898ff7ad1c4014f1931b4ed0ff
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-03-09 23:07:01 +0500

    Palindrome Checker added

--- strings/PalindromeStrings.java
+++ strings/PalindromeStrings.java
@@ -5,8 +5,9 @@ public class PalindromeStrings {
     public static boolean IsPalindrome(String str){
 
         int end = str.length()-1;
+        System.out.println(str.length());
 
-        for(int i = 0; i < end/2; i++){
+        for(int i = 0; i < end/2+1; i++){
 
             if(str.charAt(i) != str.charAt(end-i)){
                 return false;
@@ -17,7 +18,7 @@ public class PalindromeStrings {
 
     public static void main(String args[]){
 
-        String str = "abcdlcba";
+        String str = "abcdedcba";
 
         if(IsPalindrome(str))
             System.out.println("The string is Palindrome");
