e4a51be4c1782a64f983b3ef61931728b0570041
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-03-10 23:38:23 +0500

    second time of find duplicate in array

--- Arrays/FindTheDuplicate.java
+++ Arrays/FindTheDuplicate.java
@@ -1,13 +1,27 @@
 package Arrays;
 
+import java.util.Set;
+import java.util.HashSet;
+
+
 public class FindTheDuplicate {
 
     public static void main(String args[]){
 
+        Set<Integer> set = new HashSet<>();
+
         int arr[] = {1,3,4,2,2};
 
         for(int i = 0; i < arr.length; i++){
-            
+            set.add(arr[i]);
+        }
+
+        System.out.println(set);
+
+        for(int i = 0; i < arr.length; i++){
+            if(set.contains(arr[i]) == false){
+                System.out.println(arr[i]);
+            }
         }
     }
     
