0450df7b02d376ac7205995592dec4fda97364e6
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-03-15 22:25:39 +0500

    Practice file updated

--- Arrays/PractRough.java
+++ Arrays/PractRough.java
@@ -1,26 +1,29 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class PractRough {
     public static void main(String[] args) {
 
         int[] arr = {9, 1, 0, 3, 4, 5};
 
-        int largest = Integer.MIN_VALUE ;
-        int secondLargest = Integer.MIN_VALUE;
-
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] > largest){
-                secondLargest = largest;
-                largest = arr[i];
-            }else if(arr[i] > secondLargest && arr[i] != largest){
-                secondLargest = arr[i];
-            }
-
+        int left = 0;
+        int right = arr.length-1;
+        while(left < right){
+            int temp = arr[left];
+            
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
         }
-        System.out.println("The largest number in the array is: " + largest);
-        System.out.println("The second largest number in the array is: " + secondLargest);
 
+         System.out.println("The array after reversing is: ");
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i] + " ");
+        // }
 
+         System.out.println("Reversed: " + Arrays.toString(arr));
     }
     
 }
