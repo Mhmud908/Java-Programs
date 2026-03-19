e8bf9a56c175c2ea131b49675bc8437f60f676a6
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-03-19 22:51:03 +0500

    Subarray Sum Equals k2 added and modified

--- Arrays/SubarraySumEqualsK.java
+++ Arrays/SubarraySumEqualsK.java
@@ -1,6 +1,9 @@
 package Arrays;
 
+import java.util.HashMap;
+
 public class SubarraySumEqualsK {
+
     public static void main(String[] args) {
         int[] nums = {1, 1, 1};
         int k = 2;
@@ -8,16 +11,24 @@ public class SubarraySumEqualsK {
     }
 
     public static int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // base case
+
+        int prefixSum = 0;
         int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                if (sum == k) {
-                    count++;
-                }
+
+        for (int num : nums) {
+
+            prefixSum += num;
+
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
             }
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
+
         return count;
     }
 }
