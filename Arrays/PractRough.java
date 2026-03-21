f84211a4ce2da239c50bbef5a1d2de2ca5742e18
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-03-21 23:55:07 +0500

    Daily practice Remove number from string

--- Arrays/PractRough.java
+++ Arrays/PractRough.java
@@ -5,30 +5,20 @@ import java.util.Arrays;
 public class PractRough {
     public static void main(String[] args) {
 
-        int[] arr = { 9, 1, 0, 3, 4, 5 };
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
 
-        ///// resvese array
+        String str = "abc12def90";
 
-        int left = 0;
-        int right = arr.length - 1;
-        while (left < right) {
-            int temp = arr[left];
+        ///// Remove numbers from String
 
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
-        }
+        str = str.replaceAll("[0-9]", "");
+        System.out.println(str);
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+
+        
+        
+
+
 
     }
 
