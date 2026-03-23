2d6fd6d2ca9520c9955db1a37a6b0304b80cbf19
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-03-23 18:06:30 +0500

    pushing zeros to end in practice

--- Arrays/PractRough.java
+++ Arrays/PractRough.java
@@ -13,44 +13,27 @@ public class PractRough {
 
 
         // Example Input
-        int[] nums = {-1, 0, 1, 2, -1, -4};
-        
-        // 1. Sort the array - Crucial for the two-pointer approach
-        Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
+        int[] nums = {1, 0, 1, 2, 0, 5, 0, 9}; 
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Optimization: If the smallest number is > 0, no triplet can sum to 0
-            if (nums[i] > 0) break;
+        int insertPos = 0;
+        System.out.println(nums[0]);
 
-            // Skip duplicate values for the first element to ensure unique triplets
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    
-                    // Skip duplicates for the left and right pointers
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++; // Sum too small, move rightward
-                } else {
-                    right--; // Sum too large, move leftward
-                }
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0 || nums[i] < 0 || nums[i] > 0) {
+                // System.out.println(nums[i]);
+                nums[insertPos] = nums[i];
+                insertPos++;
             }
         }
 
-        // Print the results
-        System.out.println("Unique Triplets: " + result);
+        for(int i = insertPos; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        
 
         
         
