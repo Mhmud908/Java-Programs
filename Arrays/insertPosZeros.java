d3aca0e021c6d465ee606ce13ac758d939f5d517
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-03-24 23:21:01 +0500

    InsertPosZeros added

---
+++ Arrays/insertPosZeros.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class insertPosZeros {
+    public static void main(String[] args) {
+
+
+                int[] nums = {1, 0, 1, 2, 0, 5, 0, 9}; 
+
+        int insertPos = 0;
+        System.out.println(nums[0]);
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0 || nums[i] < 0 || nums[i] > 0) {
+                // System.out.println(nums[i]);
+                nums[insertPos] = nums[i];
+                insertPos++;
+            }
+        }
+
+        for(int i = insertPos; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
