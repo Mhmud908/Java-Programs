15e74ebb841f6c97f5b64cdb39c1019d93a1f96c
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-03-25 21:56:39 +0500

    Remove characters from string added

--- Arrays/ArraysPractice.java
+++ Arrays/ArraysPractice.java
@@ -2,8 +2,10 @@ package Arrays;
 
 class ArraysPractice {
     public static void main(String[] args) {
-        ////// fabonachi
-        
+        ////// remove characters from a string
+        String str = "Hello, World!";
+        str = str.replaceAll("l", "");
+        System.out.println(str);
         
     }
     
