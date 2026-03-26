f6d1523abaf220f63ced6526261b3b1059f32785
Mehmood Hassan <mhmudhassan658@gmail.com>
2026-03-26 21:56:00 +0500

    Hare and tortoise practice

--- Arrays/PractRough.java
+++ Arrays/PractRough.java
@@ -11,26 +11,21 @@ public class PractRough {
 
 
         // Example Input
-        int[] arr = {1, 0, 1, 2, 0, 5, 0, 9}; 
+        // int[] arr = {1, 0, 1, 2, 0, 5, 0, 9}; 
 
 
-        int insertPos = 0;
-        ///// for non zeros
-         for(int i = 0; i < arr.length; i++){
-            if(arr[i] != 0){
-                arr[insertPos++] = arr[i];
-            }
-         }
+
+        int[] arr = {1,3,4,2,2};
+
+        int slow = arr[0];
+        // int fast = arr[0];
+
+        // Phase 1: Detect cycle
         
-         ///// for zeros
-         for(int i = insertPos; i < arr.length; i++){
-                arr[i] = 0;
-         }
-
-         System.out.println(Arrays.toString(arr));
-
-        
-        
+            System.out.println(arr[slow]);
+            System.out.println("between");
+            System.out.println(arr);
+            // System.out.println(arr[arr[fast]]);
 
 
 
